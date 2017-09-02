e314c6029b2261c5b4e916f84fab13afdde970f6
uptickr <iy3rvashistha@gmail.com>
2017-09-02 13:58:11 +0530

    Create adjusted_ohlcv.R

---
+++ adjusted_ohlcv.R
@@ -0,0 +1,22 @@
+library(httr)
+library(stringr)
+library(dplyr)
+library(quantmod)
+
+URLS <- c("https://www.nseindia.com/marketinfo/companyTracker/compInfo.jsp?symbol=%s&series=EQ",
+          "http://www.moneycontrol.com/mccode/common/autosuggesion.php?query=%s&type=1&format=json",
+          "http://www.moneycontrol.com/tech_charts/nse/his/%s.csv")
+
+getData <- function(symbol) {
+  ISIN <- GET(sprintf(URLS[1], symbol)) %>% content("text") %>% str_match("ISIN : </b>(.*)</td>") %>% .[2]
+  MC <- GET(sprintf(URLS[2], ISIN)) %>% content("parsed") %>% .[[1]] %>% .$sc_id %>% tolower()
+  CSV <- GET(sprintf(URLS[3], MC)) %>% content("text", encoding = "ISO-8859-1") %>% 
+    read_csv(col_names = c("Date", "Open", "High", "Low", "Close", "Volume", "Bonus", "Dividends", "Rights", "Splits")) %>%
+    mutate(Date = as.POSIXct(Date, format = "%d %b %Y"))
+  return(CSV)
+}
+
+symbol <- "BAJAJ-AUTO"
+df <- getData(symbol)
+
+barChart(df %>% as_xts, name=symbol, subset='2016::', theme='white.mono', bar.type='hlc')
